8726e3f068557f5ac30de33e6e53d41db4d39b72
Salim-amir <salimamir2005@gmail.com>
2025-02-19 09:04:40 +0700

    Percobaan 2 & 3

--- Mahasiswa16.java
+++ Mahasiswa16.java
@@ -5,6 +5,16 @@ public class Mahasiswa16 {
     String kelas;
     double ipk;
 
+    public Mahasiswa16() {
+
+    }
+    public Mahasiswa16(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
@@ -34,5 +44,8 @@ public class Mahasiswa16 {
         else {
             return "Kinerja kurang";
         }
+
+    
+    
 }
 }
--- MahasiswaMain16.java
+++ MahasiswaMain16.java
@@ -5,48 +5,61 @@ public class MahasiswaMain16 {
     String kelas;
     double ipk;
 
+    public MahasiswaMain16() {
+    }
+
+    public MahasiswaMain16(String nama, String nim, double ipk, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kelas;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("Kelas: " + kelas);
         System.out.println("IPK: " + ipk);
     }
+
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
+
     void updateIpk(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0){
-        ipk = ipkBaru;
-         } else {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
     }
+
     String nilaiKinerja() {
-        if ( ipk >= 3.5) {
+        if (ipk >= 3.5) {
             return "Kinerja sangat baik";
-        }
-        else if (ipk >= 3.0) {
+        } else if (ipk >= 3.0) {
             return "Kinerja baik";
-        }
-        else if (ipk >= 2.0) {
+        } else if (ipk >= 2.0) {
             return "Kinerja cukup";
-        }
-        else {
+        } else {
             return "Kinerja kurang";
         }
     }
-    public static void main(String[] args) {
-        
-    
-    Mahasiswa16 mhs1 = new Mahasiswa16();
-    mhs1.nama = "Muhammad Ali Farhan";
-    mhs1.nim = "2241720171";
-    mhs1.kelas = "SI 2J";
-    mhs1.ipk = 3.55;
 
-    mhs1.tampilkanInformasi();
-    mhs1.ubahKelas("SI 2K");
-    mhs1.updateIpk(3.60);
-    mhs1.tampilkanInformasi();
+    public static void main(String[] args) {
+        MahasiswaMain16 mhs1 = new MahasiswaMain16();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+
+        MahasiswaMain16 mhs2 = new MahasiswaMain16("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
